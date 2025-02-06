cc67cacd1920964f251b7535fada4744f3b281ef
L200160149 <ums.informatika@gmail.com>
2025-02-06 21:15:02 +0700

    init

---
+++ src/test/java/dewaning/try_spring_restful_api/TryProjectForSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dewaning.try_spring_restful_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryProjectForSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
